d57b5475e63b9dcc8ca184423ef605adfd505ba8
jonathalimax <jonathalimax@gmai.com>
2019-10-29 03:24:19 -0300

    Add https

--- setup/wakeup-timer.js
+++ setup/wakeup-timer.js
@@ -8,7 +8,7 @@ module.exports = {
         
         var http = require("http");
         setInterval(function () {
-            http.get("http://board-class.herokuapp.com");
+            http.get("https://board-class.herokuapp.com/")
         }, 300000); // every 5 minutes (300000)
 
     }
