7b1d6e346471a4ca5020abcc77aef3cb66da3c3a
jonathalimax <jonathalimax@gmai.com>
2019-12-24 19:03:11 -0300

    Change file

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,9 +1,18 @@
+// module.exports = {
+//     dialect: 'mysql',
+//     uri: process.env.JAWSDB_URL || 'mysql://root:@localhost/board_class',
+//     host: 'localhost',
+//     username: 'root',
+//     database: 'board_class',
+//     define: {
+//         timestamps: true,
+//         underscored: true
+//     }
+// }
+
 module.exports = {
     dialect: 'mysql',
-    uri: process.env.JAWSDB_URL || 'mysql://root:@localhost/board_class',
-    host: 'localhost',
-    username: 'root',
-    database: 'board_class',
+    uri: process.env.JAWSDB_URL,
     define: {
         timestamps: true,
         underscored: true
