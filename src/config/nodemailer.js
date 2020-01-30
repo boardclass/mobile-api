d75d9e58f865e7e24ddcb8285b274e41d908647c
jonathalimax <jonathalimax@gmai.com>
2020-01-30 14:11:06 -0300

    Add log to mailer

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -2,13 +2,16 @@ const nodemailer = require('nodemailer')
 
 exports.transporter = function() {
   
+    console.log(process.env.USER_EMAIL);
+    console.log(process.env.USER_EMAIL_PASSWORD);
+
     return nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 587,
         secure: false,
         auth: {
-            user: process.env.USER_EMAIL,
-            pass: process.env.USER_EMAIL_PASSWORD
+            user: process.env.USER_EMAIL || "lifestyleboardapp@gmail.com",
+            pass: process.env.USER_EMAIL_PASSWORD || "12Boardapp!@#$"
         }
     })
     
