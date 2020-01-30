e1ef89e260947392a953f8c7f5fea3672011565d
jonathalimax <jonathalimax@gmai.com>
2020-01-30 10:46:06 -0300

    Add secury level

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -5,7 +5,7 @@ exports.transporter = function() {
     return nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 587,
-        secure: false,
+        secure: true,
         auth: {
             user: 'lifestyleboardapp@gmail.com',
             pass: '12Boardapp!@#$'
