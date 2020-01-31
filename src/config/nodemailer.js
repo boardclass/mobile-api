d79972bf81f098b38100fc84d80f85dcec5f7f73
Jonatha Lima <devjonh@gmail.com>
2020-01-30 21:33:24 -0300

    Remove log and and secure to smtp connection

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,14 +1,11 @@
 const nodemailer = require('nodemailer')
 
 exports.transporter = function() {
-  
-    console.log(process.env.USER_EMAIL);
-    console.log(process.env.USER_EMAIL_PASSWORD);
 
     return nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 587,
-        secure: false,
+        secure: true,
         auth: {
             user: process.env.USER_EMAIL,
             pass: process.env.USER_EMAIL_PASSWORD
