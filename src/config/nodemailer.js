6fc4d755529fb39c1744fe4ec5d1cc118407b2db
Jonatha Lima <devjonh@gmail.com>
2020-01-31 00:57:52 -0300

    Remove secutiry from transporter

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -5,7 +5,7 @@ exports.transporter = function() {
     return nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 587,
-        secure: true,
+        secure: false,
         auth: {
             user: process.env.USER_EMAIL,
             pass: process.env.USER_EMAIL_PASSWORD
