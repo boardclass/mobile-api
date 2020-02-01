1129217264ff52b6e62eeadf0d67c47855378509
jonathalimax <jonathalimax@gmai.com>
2020-02-01 17:24:50 -0300

    Add log and tsl config

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -2,10 +2,13 @@ const nodemailer = require('nodemailer')
 
 exports.transporter = function() {
 
+    console.log(process.env.USER_EMAIL);
+    console.log(USER_EMAIL_PASSWORD);
+    
     return nodemailer.createTransport({
         host: 'smtp.zoho.com',
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.USER_EMAIL,
             pass: process.env.USER_EMAIL_PASSWORD
