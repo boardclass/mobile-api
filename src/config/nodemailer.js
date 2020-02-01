e99d0ad347a652103f89a3b934c9180878f6e356
jonathalimax <jonathalimax@gmai.com>
2020-02-01 17:09:53 -0300

    Add ssl config to nodemailer

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -4,8 +4,8 @@ exports.transporter = function() {
 
     return nodemailer.createTransport({
         host: 'smtp.zoho.com',
-        port: 587,
-        secure: false,
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.USER_EMAIL,
             pass: process.env.USER_EMAIL_PASSWORD
