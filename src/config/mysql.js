4908117636266f0bf88131b83aa821015f8a68b2
Jonatha Lima <devjonh@gmail.com>
2020-02-24 13:06:15 -0300

    Change database connection to pool

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -3,9 +3,12 @@ const mysql = require('mysql2')
 module.exports = {
     dialect: 'mysql',
     uri: process.env.JAWSDB_URL || 'mysql://root:@localhost/board_class',
-    host: 'localhost',
-    username: 'root',
-    database: 'board_class',
+    host: process.env.SQL_HOST || 'localhost',
+    username: process.env.SQL_USERNAME || 'root',
+    password: process.env.SQL_PASSWORD || '',
+    database: process.env.SQL_DATABASE || 'board_class',
+    connectionLimit: 10,
+
     define: {
         timestamps: true,
         underscored: true
@@ -13,11 +16,16 @@ module.exports = {
 
     connect: function (uri, callback) {
 
-        var connection = mysql.createConnection(uri)
+        var connection = mysql.createPool({
+            connectionLimit: this.connectionLimit,
+            host: this.host,
+            user: this.username,
+            password: this.password,
+            database: this.database,
+            port: this.port
+        })
 
-        connection.connect()
         callback(connection)
 
     }
 }
-
