a8ec72464f4b86369798b4844edb168409b1eeb8
Jonatha Lima <devjonh@gmail.com>
2020-04-25 22:24:45 -0300

    fix

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -555,7 +555,7 @@ exports.agenda = async function (req, res) {
                 WHERE 
                     s.user_id = ?
                     AND s.status_id NOT IN (?)
-                GROUP BY b.id
+                GROUP BY b.id, date
                 ORDER BY date, sport, establishment, start_hour
                 `
 
