24ff49368e28e57075a3b18da49e19dc62ff7ab0
jonatha.lima <jonatha.lima@dextra-sw.com>
2020-12-05 18:08:53 -0300

    update request timer wake-up

--- src/config/wakeup-timer.js
+++ src/config/wakeup-timer.js
@@ -5,9 +5,8 @@ module.exports = {
     setTimer: function () {
         
         setInterval(function () {
-            https.get("https://board-class.herokuapp.com/")
             https.get("https://board-class-develop.herokuapp.com/")
-        }, 300000); // every 5 minutes (300000)
+        }, 900000); // every 15 minutes (900000)
 
     }
 
