03523fee16c7a6ad654a08fd47de447beb859ff1
jonatha.lima <jonatha.lima@dextra-sw.com>
2021-01-06 19:39:30 -0300

    change mailer lib

--- src/api/classes/mailer.js
+++ src/api/classes/mailer.js
@@ -1,31 +1,39 @@
-const nodemailer = require('nodemailer')
-const mailGun = require('nodemailer-mailgun-transport')
-
 module.exports = {
 
     send: function (data, callback) {
 
-        const auth = {
-            auth: {
-                api_key: "b71d032b4eb737544c8c78724f5a7b56-074fa10c-849b62d2",
-                domain: "sandbox67f74dc1590a4f7a92b78e4372c54cc9.mailgun.org"
-            }
-        }
-
-        const transporter = nodemailer.createTransport(mailGun(auth))
-
-        transporter.sendMail({
-            from: `Board Class <atendimento@boardclass.com.br>`,
+        const send = require('gmail-send')({
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASSWORD,
             to: data.destination,
             subject: data.subject,
+        });
+
+        send({
             text: data.message,
-            attachments: data.attachments
-        }).then(message => {
-            callback(message)
-        }).catch(err => {
-            console.log(err)
-            callback(null)
+            files: [data.attachments]
+        }, (error, result, fullResult) => {
+            if (error) {
+                console.log(err)
+                return callback(null)
+            }
+
+            console.log(result);
+            return callback(result)
         })
+
+        // transporter.sendMail({
+        //     from: `Board Class <atendimento@boardclass.com.br>`,
+        //     to: data.destination,
+        //     subject: data.subject,
+        //     text: data.message,
+        //     attachments: data.attachments
+        // }).then(message => {
+        //     callback(message)
+        // }).catch(err => {
+        //     console.log(err)
+        //     callback(null)
+        // })
 
     }
 
