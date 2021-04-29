88f59af7b825fcc3d43f1e6da069d2e8d4f15713
Jonatha Lima <jonathalimax@gmail.com>
2021-04-28 21:34:42 -0300

    Remove async from self schedule request

--- src/api/v2/controllers/establishmentController.js
+++ src/api/v2/controllers/establishmentController.js
@@ -1750,7 +1750,9 @@ exports.selfSchedule = async function (req, res) {
             if (err)
                 return handleError(req, res, 500, "Ocorreu um erro no agendamento!", err)
 
-            console.log(result);
+            console.log(result[1][0].callback);
+            console.log('---');
+            console.log(result[2][0].callback);
 
             if (result[2][0] != null) {
                 return res.status(500).json({
