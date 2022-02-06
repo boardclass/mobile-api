1342e675ecd1f1a61cd5c7397024e99faa874a28
jonatha.lima <jonatha.pereira_dextra@prestador.globo>
2022-02-06 15:53:15 -0300

    Add ssl configuration

--- src/config/server.js
+++ src/config/server.js
@@ -1,58 +1,61 @@
-require('./sequelize')
+require("./sequelize");
 
-const https = require('https')
-const path = require('path')
-const fs = require('fs')
-const express = require('express')
-const validator = require('express-validator')
-const bodyParser = require('body-parser')
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+const validator = require("express-validator");
+const bodyParser = require("body-parser");
 
-const pool = require('../config/database');
-const middleware = require('./middleware')
-const connectionMiddleware = require('./connectionMiddleware')
-const excludedRoutes = require('./excludedRoutes')
-const mongodb = require('./mongodb')
+const pool = require("../config/database");
+const middleware = require("./middleware");
+const connectionMiddleware = require("./connectionMiddleware");
+const excludedRoutes = require("./excludedRoutes");
+const mongodb = require("./mongodb");
 
-const app = express()
+const app = express();
 
 module.exports = {
+  start: function () {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(validator());
 
-    start: function () {
+    app.use(unless(excludedRoutes.excluded, middleware.validateToken));
+    app.use(middleware.versioning);
+    app.use(connectionMiddleware(pool));
 
-        app.use(bodyParser.json())
-        app.use(bodyParser.urlencoded({ extended: true }))
-        app.use(validator())
+    require("./routes")(app);
 
-        app.use(unless(excludedRoutes.excluded, middleware.validateToken))
-        app.use(middleware.versioning)
-        app.use(connectionMiddleware(pool))
+    mongodb.setup();
 
-        require('./routes')(app)
+    let port = process.env.PORT_INDEX || 8080;
 
-        mongodb.setup()
+    console.log(`process.env.PORT ${process.env.PORT}`);
+    console.log(`process.env.PORT_INDEX ${process.env.PORT_INDEX}`);
+    console.log(`process.env.PORT_SERVER ${process.env.PORT_SERVER}`);
+    console.log(`process.env.PORT_APP ${process.env.PORT_APP}`);
 
-        let port = process.env.PORT_INDEX || 8080
-        
-        console.log(`process.env.PORT ${process.env.PORT}`);
-        console.log(`process.env.PORT_INDEX ${process.env.PORT_INDEX}`);
-        console.log(`process.env.PORT_SERVER ${process.env.PORT_SERVER}`);
-        console.log(`process.env.PORT_APP ${process.env.PORT_APP}`);
+    const sslServer = https.createServer(
+      {
+        key: fs.readFileSync(path.join(__dirname, 'cert', 'ssl', 'key.pem')),
+        cert: fs.readFileSync(path.join(__dirname, 'cert', 'ssl', 'cert.pem')),
+      },
+      app
+    );
 
-        app.listen(port, () => {
-            console.log(`Server started on ${port}`)
-        })
-
-    }
-
-}
+    sslServer.listen(port, () => {
+      console.log(`Secure server started on ${port}`);
+    });
+  },
+};
 
 const unless = function (paths, middleware) {
-    return function (req, res, next) {
-
-        if (paths.includes(req.path)) {
-            return next()
-        } else {
-            return middleware(req, res, next)
-        }
+  return function (req, res, next) {
+    if (paths.includes(req.path)) {
+      return next();
+    } else {
+      return middleware(req, res, next);
     }
-}
+  };
+};
