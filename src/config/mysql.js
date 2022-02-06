6963a22164cca9ba8c73e8baae2e553ffd219a35
jonatha.lima <jonatha.pereira_dextra@prestador.globo>
2022-02-06 09:13:33 -0300

    Change mysql password

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'mysql',
-    uri: process.env.JAWSDB_URL || 'mysql://boardclass:Bo@rd@app@mysql.boardclass.kinghost.net:3306/boardclass',
+    uri: process.env.JAWSDB_URL || 'mysql://boardclass:Bo@rd@pp@mysql.boardclass.kinghost.net:3306/boardclass',
     host: process.env.SQL_HOST || 'mysql.boardclass.kinghost.net',
     username: process.env.SQL_USERNAME || 'boardclass',
-    password: process.env.SQL_PASSWORD || 'Bo@rd@app',
+    password: process.env.SQL_PASSWORD || 'Bo@rd@pp',
     database: process.env.SQL_DATABASE || 'boardclass',
     connectionLimit: 10,
     port: process.env.SQL_PORT || 3306,
